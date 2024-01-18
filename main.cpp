963f49b6c7e6634e87b6e9d2496360d5e3b9ae2e
retobe <retobeyt@gmail.com>
2024-01-17 20:23:08 -0500

    main file

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+
+    cout << "Write down a number: ";
+    cin >> x;
+    cout << "Write down another number: ";
+    cin >> y;
+    cout << "Your numbers added are: " << x + y;
+    return 0;
+}
